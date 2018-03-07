4d5d956353977d3265ba319bffee67d6b031a2b6
fshenma <frankyshen@gmail.com>
2018-03-07 11:22:08 -0500

    inti todos-native-test

---
+++ constants/Config.js
@@ -0,0 +1,7 @@
+// all the app configurations
+export default {
+    url: 'https://scalr.api.appbase.io',
+    app: 'todos-native-f',
+    credentials: 'zkcTnMWXx:597b6149-0716-4484-a211-45711f6f0e93',
+    type: 'todos-native',
+};
